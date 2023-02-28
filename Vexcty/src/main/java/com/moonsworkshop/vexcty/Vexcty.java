e8a6717242e989de7b077152db0f873bcacc26df
Taha Akhtar <sunseedyay@gmail.com>
2023-02-28 20:12:10 +0000

    just check what i changed because i kinda forgot

--- Vexcty/src/main/java/com/moonsworkshop/vexcty/Vexcty.java
+++ Vexcty/src/main/java/com/moonsworkshop/vexcty/Vexcty.java
@@ -1,95 +1,300 @@
 package com.moonsworkshop.vexcty;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
+import com.moonsworkshop.vexcty.afk.AFKManager;
+import com.moonsworkshop.vexcty.afk.MovementChecker;
 import com.moonsworkshop.vexcty.command.PunishCommand;
-import com.moonsworkshop.vexcty.command.ReportCommand;
 import com.moonsworkshop.vexcty.command.TestCommand;
 import com.moonsworkshop.vexcty.event.PlayerEvent;
+import com.moonsworkshop.vexcty.lang.Lang;
+import com.moonsworkshop.vexcty.lang.LangCommand;
 import com.moonsworkshop.vexcty.lang.LangManager;
-import com.moonsworkshop.vexcty.managers.*;
-import com.moonsworkshop.vexcty.rank.*;
+import com.moonsworkshop.vexcty.managers.PlayerManager;
+import com.moonsworkshop.vexcty.rank.NametagManager;
+import com.moonsworkshop.vexcty.rank.RankCommand;
+import com.moonsworkshop.vexcty.rank.RankManager;
+import com.moonsworkshop.vexcty.sql.ProfileManager;
+import com.moonsworkshop.vexcty.sql.SQLManager;
+import com.moonsworkshop.vexgot.Board;
+import com.moonsworkshop.vexgot.VexGot;
 import lombok.Getter;
-import net.minecraft.server.v1_8_R3.MinecraftServer;
-import org.bson.Document;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-public class Vexcty extends JavaPlugin {
+public class Vexcty extends VexGot {
 
-
+    public static Map<UUID, Board> boards = new HashMap<>();
 
     private static Vexcty instance;
     @Getter
     private LangManager langManager;
+//    @Getter
+//    private ItemManager item;
+    @Getter
+    private PlayerManager player;
+    @Getter
+    private AFKManager afkManager;
     @Getter
     private RankManager rankManager;
     @Getter
     private NametagManager nametagManager;
     @Getter
-    private Perks perks;
+    public SQLManager MySQLManager;
     @Getter
-    private InventoryManager inv;
-    @Getter
-    private ItemManager item;
-    @Getter
-    private PlayerManager player;
-    @Getter
-    private ReportManager reportManager;
-
-
-
+    public ProfileManager profileManager;
 
     @Override
-    public void onEnable() {
-        Player player = null;
-
+    public void PluginEnable() {
         managers();
         commands();
         listeners();
+        afk();
+    }
+
+    @Override
+    public void PluginDisable() {
+
     }
 
     public void managers() {
+//        item = new ItemManager();
+        player = new PlayerManager();
+        langManager = new LangManager(this);
+        afkManager = new AFKManager();
         rankManager = new RankManager(this);
         nametagManager = new NametagManager(this);
-        perks = new Perks(this);
-        inv = new InventoryManager();
-        item = new ItemManager();
-        player = new PlayerManager();
-        reportManager = new ReportManager();
-        langManager = new LangManager(this);
+        MySQLManager = new SQLManager();
+        profileManager = new ProfileManager();
 
     }
 
     public void commands() {
-        Arrays.asList(
-        new ReportCommand()).forEach(command -> this.bukkitCommand(getName(), command));
-        getCommand("rank").setExecutor(new RankCommand(this));
         getCommand("test").setExecutor(new TestCommand());
         getCommand("punish").setExecutor(new PunishCommand(this));
-        getCommand("report").setExecutor(new ReportCommand(this));
+        getCommand("rank").setExecutor(new RankCommand(this));
+        getCommand("lang").setExecutor(new LangCommand(this));
     }
 
     public void listeners() {
-        Listener PlayerEvent = (Listener) new PlayerEvent(this);
-        Listener RankListener = (Listener) new RankListener(this);
-        Bukkit.getPluginManager().registerEvents(PlayerEvent, this);
-        Bukkit.getPluginManager().registerEvents(RankListener, this);
+        Bukkit.getPluginManager().registerEvents(new PlayerEvent(this, new SQLManager()), this);
     }
 
     public static Vexcty getInstance() {
         return Vexcty.instance;
     }
 
-    private void bukkitCommand(String prefix, Command command) {
-        MinecraftServer.getServer().server.getCommandMap().register(prefix, command);
+        public void afk() {
+            Bukkit.getScheduler().runTaskTimerAsynchronously(this, new MovementChecker(this.afkManager), 0L, 600L);
+
+            this.afkManager = new AFKManager();
     }
 
+    public void scoreboard(Player player) {
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            for (Board board : boards.values()) {
+                updateBoard(board, player);
+            }
+        }, 0, 60);
+    }
+
+    private void updateBoard(Board board, Player player) {
+
+        if (getLangManager().getLang(player.getUniqueId(), Lang.EN)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Players: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Rank: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.AR)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "اللاعبين: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "مرتبة: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.FR)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Joueurs: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Rang: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.IT)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Giocatori: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Rango: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.JA)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "プレイヤー: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "ランク: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.LA)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Histriones: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Rank: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.PA)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "ਖਿਡਾਰੀ: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "ਰੈਂਕ: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.RU)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Игроки: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Классифицировать: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.ES)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Jugadores: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Rango: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.TR)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "oyuncular: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Rütbe: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.UR)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "کھلاڑی: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "رینک: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.AM)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "ተጫዋቾች: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "ደረጃ: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.ZH)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "球员: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "秩: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.ZHHK)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "球员: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "秩: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.CS)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Hráči: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Hodnost: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.DE)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Spieler: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Rang: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.FI)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Pelaajat: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Sijoitus: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.PT)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Jogadoras: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Classificação: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        } else if (getLangManager().getLang(player.getUniqueId(), Lang.PT)) {
+            board.updateLines(
+                    ChatColor.GRAY + String.valueOf(player.getWorld()),
+                    "",
+                    "Gracze: " + getServer().getOnlinePlayers().size(),
+                    "",
+                    "Ranga: " + getRankManager().getRank(player.getUniqueId()).getName(),
+                    "",
+                    ChatColor.YELLOW + "www.vexcty.com"
+            );
+        }
+
+    }
 }
