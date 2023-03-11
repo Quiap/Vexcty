4dcfa551fce6c1911c87d8964e5caa5bdb46062d
Taha Akhtar <sunseedyay@gmail.com>
2023-03-11 17:28:50 +0000

    added some ranks

--- Vexcty/src/main/java/com/moonsworkshop/vexcty/rank/PlayerRank.java
+++ Vexcty/src/main/java/com/moonsworkshop/vexcty/rank/PlayerRank.java
@@ -5,16 +5,17 @@ import lombok.Getter;
 
 public enum PlayerRank {
 
-    OWNER(CC.DARK_RED + "[OWNER]", 13, true),
-    CO_OWNER(CC.DARK_RED + "[CO-OWNER]", 12, true),
-    MANAGER(CC.YELLOW + "[MANAGER]", 11, true),
-    ADMIN(CC.RED + "[ADMIN]", 10, true),
-    MOD(CC.DARK_GREEN + "[MODERATOR]", 9, true),
-    DEV(CC.BLACK + "[DEVELOPER]", 8, true),
-    YOUTUBE(CC.RED + "[YOUTUBE]", 7 ,false),
-    TWITCH(CC.L_PURPLE + "[TWITCH]", 6, false),
-    MOJANG(CC.GOLD + "[MOJANG]", 5, false),
-    LEGEND(CC.BLACK + "[LEGEND]", 4, false),
+    OWNER(CC.DARK_RED + "[OWNER]", 14, true),
+    MANAGER(CC.YELLOW + "[MANAGER]", 13, true),
+    ADMIN(CC.RED + "[ADMIN]", 12, true),
+    MOD(CC.DARK_GREEN + "[MODERATOR]", 11, true),
+    DEV(CC.BLACK + "[DEVELOPER]", 10, true),
+    YOUTUBE(CC.RED + "[YOUTUBE]", 9,false),
+    TWITCH(CC.L_PURPLE + "[TWITCH]", 8, false),
+    MOJANG(CC.GOLD + "[MOJANG]", 7, false),
+    LEGEND(CC.BLACK + "[LEGEND]", 6, false),
+    HACKER(CC.DARK_RED + "[HACKER]", 5, false),
+    NOOB(CC.YELLOW + "[NOOB]", 4, false),
     DONOR(CC.AQUA + "[DONOR]", 3, false),
     VOTER("⚔︎", 2, false),
     MEMBER(CC.GRAY + "", 1, false);
@@ -25,12 +26,24 @@ public enum PlayerRank {
     @Getter
     private int level;
     @Getter
-    private boolean isStaffRank;
+    public boolean isStaffRank;
+
+    @Getter
+    public static boolean isStaffRankk;
 
     PlayerRank(String name, int level, boolean isStaffRank) {
         this.name = name;
         this.level = level;
         this.isStaffRank = isStaffRank;
+    }
+
+    public boolean isStaffRank(boolean isStaffRank) {
+
+        if (PlayerRank.isStaffRankk() == true) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
